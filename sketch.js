6e00ad291a40025b8606c77c08e5da6eef38aa41
navin433 <85050627+navin433@users.noreply.github.com>
2021-07-16 17:19:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 110, 50, angle);
+  cannonBall = new cannonBall(cannon.x,cannon.y)
+
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  
+
+  Engine.update(engine);
+  ground.display();
+  for (var i=0; i < ball.length;i++){
+    showCannonBalls(balls[i],i)
+  }
+  
+
+  cannon.display();
+  tower.display();
+  //cannonBall.display();
+ 
+}
+function keyReleased(){
+  if (keycode===DOWN_ARROW){
+    balls[balls.length-1].shoot();
+
+  }
+}
+
+function showCannonBalls(ball,index){
+ball.display();
+if(ball.body.position.x>=width||ball.body.position.y >= height-50){
+  Matter.world.remove(world,ball.body)
+  balls.splice(index,1)
+}
+}
